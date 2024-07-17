dabe22bd524449050209629abcb1ee75b5d44310
Val <68100216+xVal429@users.noreply.github.com>
2024-07-16 20:59:07 -0500

    Update v_chatmodifier.lua
    Localization.

--- v_chatmodifier.lua
+++ v_chatmodifier.lua
@@ -1,21 +1,28 @@
 local PLUGIN = PLUGIN
 PLUGIN.name = "Chat Prefix & Suffix"
 PLUGIN.author = "Val"
-PLUGIN.description = "Pick up that can."
+PLUGIN.description = "Allows players to set a custom prefix and suffix for their chat messages. /chatmodifier [prefix] [suffix] to set, /chatmodifier \"\" \"\" to disable."
 PLUGIN.chatTypes = {"ic", "w", "y", "dispatch"}
 PLUGIN.invalidModifiers = {"", "nil", "false"} -- Effectively disables the modifier if the prefix or suffix is one of these values. Can be used to blacklist certain prefixes or suffixes if needed.
 
-ix.command.Add("chatmodifier", {
-    description = "Set a custom prefix and suffix for your messages. Leave the arguments empty to disable the modifier.",
+ix.lang.AddTable("english", {
+    cmdChatModifier = "Set a custom prefix and suffix for your messages. /ChatModifier \"\" \"\" to disable.",
+    cmdChatModifierEnabled = "You have set your chat modifier.",
+    cmdChatModifierExample = "%s This is an example message. %s",
+    cmdChatModifierDisabled = "You have disabled the chat modifier."
+})
+
+ix.command.Add("ChatModifier", {
+    description = "@cmdChatModifier",
     arguments = { ix.type.string, ix.type.string },
     OnRun = function(self, client, prefix, suffix)
         if prefix and suffix and not table.HasValue(PLUGIN.invalidModifiers, prefix) and not table.HasValue(PLUGIN.invalidModifiers, suffix) then
             client:SetVar("chatModifier", {prefix = prefix, suffix = suffix})
-            client:Notify("You have set your chat modifier.")
-            client:Notify(prefix .. " This is an example message. " .. suffix)
+            client:NotifyLocalized("cmdChatModifierEnabled")
+            client:NotifyLocalized("cmdChatModifierExample", prefix, suffix)
         else
             client:SetVar("chatModifier", nil)
-            client:Notify("You have disabled the chat modifier.")
+            client:NotifyLocalized("cmdChatModifierDisabled")
         end
     end
 })
